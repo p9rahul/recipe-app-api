7d7af77e7459a5205cce152bff2166cf333cd3f5
unknown <p9rahul@gmail.com>
2024-03-03 21:41:21 +0530

    core app

---
+++ core/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    # Otherwise error will come in admin url
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info Rahul:'), {'fields': ('name',)}),
+        (
+            _('Permissions Rahul :'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (_('Important dates Rahul:'), {'fields': ('last_login',)})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
+# admin.site.register(models.Tag)
+# admin.site.register(models.Ingredient)
+# admin.site.register(models.Recipe)
