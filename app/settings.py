aa83b43fd58af618fc750e722e3fd1315cf0825a
unknown <p9rahul@gmail.com>
2024-03-03 22:53:45 +0530

    core app

--- app/settings.py
+++ app/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'rest_framework',
+    'rest_framework.authtoken',
     'drf_spectacular',
+    'user'
 ]
 
 MIDDLEWARE = [
--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,4 +15,5 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
     ),
+    path('api/user/', include('user.urls')),
 ]
